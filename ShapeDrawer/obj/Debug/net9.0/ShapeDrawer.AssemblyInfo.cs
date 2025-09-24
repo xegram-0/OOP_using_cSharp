219b83ce0ce51468ab78970922597f17264e0171
xegram-0 <darkbroodlord@gmail.com>
2025-09-24 20:52:33 +0700

    Added stuff

--- ShapeDrawer/obj/Debug/net9.0/ShapeDrawer.AssemblyInfo.cs
+++ ShapeDrawer/obj/Debug/net9.0/ShapeDrawer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShapeDrawer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b00fa2cc3cf345b0d3ffd7a5893d704d4cd5566")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a995cb2faeac5a5b3ed8ec2585646b9f109da52b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShapeDrawer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShapeDrawer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
