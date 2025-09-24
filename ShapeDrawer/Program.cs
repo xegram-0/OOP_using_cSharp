a995cb2faeac5a5b3ed8ec2585646b9f109da52b
xegram-0 <darkbroodlord@gmail.com>
2025-09-24 20:52:01 +0700

    Added ShapeDrawer

---
+++ ShapeDrawer/Program.cs
@@ -0,0 +1,22 @@
+﻿using SplashKitSDK;
+
+namespace ShapeDrawer
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Shape myShape = new Shape(176);
+            Window window = new Window("Shape Drawer", 600, 600);
+            do
+            {
+                SplashKit.ProcessEvents();
+                SplashKit.ClearScreen();
+                myShape.Draw(); 
+                
+                SplashKit.RefreshScreen();
+            } while (!window.CloseRequested);
+        }
+    }
+}
