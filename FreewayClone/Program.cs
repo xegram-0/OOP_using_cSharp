667e0b00566bfdfeff245508e4ec21d549dbecf2
Xegram <78540186+xegram-0@users.noreply.github.com>
2025-11-25 22:12:08 +0700

    Add files via upload

---
+++ FreewayClone/Program.cs
@@ -0,0 +1,27 @@
+﻿using SplashKitSDK;
+
+namespace FreewayClone
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Window window = new Window("Freeways Clone - SplashKit", 800, 600);
+            GameManager game = new GameManager();
+            
+            Color darkModeBg = Color.RGBAColor(30, 30, 35, 255); 
+
+            while (!window.CloseRequested)
+            {
+                SplashKit.ProcessEvents();
+                
+                game.HandleInput();
+                game.Update();
+                
+                window.Clear(darkModeBg);
+                game.Draw();
+                window.Refresh(60);
+            }
+        }
+    }
+}
